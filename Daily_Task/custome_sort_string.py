e0600128d0ebb86d32df77aad4da3b089b874ce2
Phav <phavm@nala.roche.com>
2024-03-11 10:17:16 -0700

    Adding problem

---
+++ Daily_Task/custome_sort_string.py
@@ -0,0 +1,14 @@
+class Solution:
+    def customSortString(self, order: str, s: str) -> str:
+        key_order = {}
+        for i,char in enumerate(order):
+            key_order[char] = i
+        sorted_string = []
+        unsorted_string = []
+        for _c in s:
+            if _c in key_order:
+                sorted_string.append((_c,key_order[_c]))
+            else:
+                unsorted_string.append(_c)
+        sorted_string = sorted(sorted_string,key=lambda x: x[1])
+        return "".join(x[0] for x in sorted_string)+"".join(unsorted_string)
